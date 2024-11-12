1bc8cde57cfb267fbab34d58858e002ad99d86c2
smter <smtersoyoglu231@gmail.com>
2024-11-12 15:23:41 +0300

    Added Hilt Application class and configure manifest for DI setup

---
+++ app/src/main/java/com/smtersoyoglu/shuffleandlearncompose/di/ShuffleAndLearnApp.kt
@@ -0,0 +1,7 @@
+package com.smtersoyoglu.shuffleandlearncompose.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ShuffleAndLearnApp : Application()
