d53ba110a5e3bffee612ea8bc71aacf0239c96b4
smter <smtersoyoglu231@gmail.com>
2024-11-12 16:27:36 +0300

    Added WordMainScreen composable with LazyVerticalGrid

---
+++ app/src/main/java/com/smtersoyoglu/shuffleandlearncompose/screens/word_main/WordMainScreen.kt
@@ -0,0 +1,67 @@
+package com.smtersoyoglu.shuffleandlearncompose.screens.word_main
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.smtersoyoglu.shuffleandlearncompose.data.model.getWordList
+import com.smtersoyoglu.shuffleandlearncompose.screens.word_main.components.WordCard
+import com.smtersoyoglu.shuffleandlearncompose.ui.theme.HeaderColor
+
+@Composable
+fun WordMainScreen(modifier: Modifier) {
+
+    val wordList = getWordList()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(top = 56.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Word Cards",
+            style = MaterialTheme.typography.headlineLarge.copy(
+                fontWeight = FontWeight.Bold,
+                color = HeaderColor
+            ),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        // Başlık altında 32dp boşluk bırakıyoruz
+        Spacer(modifier = Modifier.height(32.dp))
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            items(wordList.size) { word ->
+                WordCard(word = wordList[word])
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WordMainScreenPreview() {
+    WordMainScreen(modifier = Modifier)
+}
