fa60043c7f0ee262ef999d7b31383faff993d16e
smter <smtersoyoglu231@gmail.com>
2024-11-12 16:27:13 +0300

    Added WordCard composable for word card layout

---
+++ app/src/main/java/com/smtersoyoglu/shuffleandlearncompose/screens/word_main/components/WordCard.kt
@@ -0,0 +1,116 @@
+package com.smtersoyoglu.shuffleandlearncompose.screens.word_main.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.smtersoyoglu.shuffleandlearncompose.R
+import com.smtersoyoglu.shuffleandlearncompose.data.model.Word
+import com.smtersoyoglu.shuffleandlearncompose.ui.theme.EnglishTextColor
+import com.smtersoyoglu.shuffleandlearncompose.ui.theme.TurkishTextColor
+
+@Composable
+fun WordCard(word: Word) {
+    Box(
+        modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 12.dp)
+            .width(IntrinsicSize.Max),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Card(
+            elevation = CardDefaults.elevatedCardElevation(defaultElevation = 15.dp),
+            shape = RoundedCornerShape(18.dp),
+            colors = CardDefaults.cardColors(containerColor = Color(0xFFFFFFFF)),
+            modifier = Modifier
+                .padding(top = 30.dp)
+                .fillMaxWidth()
+                .height(230.dp)
+
+        ) {
+            Column (
+                modifier = Modifier.padding(horizontal = 15.dp, vertical = 10.dp)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(140.dp))
+
+                // Turkish translation text
+                Text(
+                    text = word.translation,
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        color = TurkishTextColor
+                    ),
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                // English word text
+                Text(
+                    text = word.english,
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 28.sp,
+                        color = EnglishTextColor
+                    ),
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+
+        Image(
+            painter = painterResource(id = word.imageUrl),
+            contentDescription = word.english,
+            modifier = Modifier
+                .size(size = 180.dp)
+                .offset(y = (-50).dp)
+                .clip(RoundedCornerShape(16.dp)),
+            contentScale = ContentScale.Fit
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WordCardPreview() {
+    WordCard(
+        word = Word(
+            1,
+            "Ördek",
+            "Duck",
+            R.drawable.duck_card,
+            "The duck is swimming in the pond.",
+        ),
+    )
+}
