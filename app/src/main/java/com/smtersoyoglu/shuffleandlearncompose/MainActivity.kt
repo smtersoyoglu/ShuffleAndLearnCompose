3537aef9417ad2185d92ce1968808500d275359f
smter <smtersoyoglu231@gmail.com>
2024-11-12 16:25:53 +0300

    Added sample images and define color palette for word cards and UI elements

--- app/src/main/java/com/smtersoyoglu/shuffleandlearncompose/MainActivity.kt
+++ app/src/main/java/com/smtersoyoglu/shuffleandlearncompose/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.smtersoyoglu.shuffleandlearncompose.screens.word_main.WordMainScreen
 import com.smtersoyoglu.shuffleandlearncompose.ui.theme.ShuffleAndLearnComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShuffleAndLearnComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    WordMainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ShuffleAndLearnComposeTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/smtersoyoglu/shuffleandlearncompose/ui/theme/Color.kt
+++ app/src/main/java/com/smtersoyoglu/shuffleandlearncompose/ui/theme/Color.kt
@@ -9,3 +9,14 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val teal_650 = Color(0xFF5196A2)
+
+val BackgroundColor = Color(0xFFF3F4F6)
+val HeaderColor = Color(0xFF3F51B5)
+val CardBackgroundColor = Color(0xFFFAF9F6)
+val TurkishTextColor = Color(0xFF424242)
+val EnglishTextColor = Color(0xFF1E88E5) // veya Color(0xFF43A047) yeşil olarak
+val BottomNavBackgroundColor = Color(0xFF3F51B5) // veya Color(0xFF512DA8) koyu mor
+val BottomNavSelectedIconColor = Color(0xFFFFFFFF)
