2d0b687872bdf0d1ab906ae6878df12e903fecbc
smter <smtersoyoglu231@gmail.com>
2024-11-13 19:17:39 +0300

    Implemented WordService to fetch words from the API

---
+++ app/src/main/java/com/smtersoyoglu/shuffleandlearncompose/data/retrofit/WordService.kt
@@ -0,0 +1,9 @@
+package com.smtersoyoglu.shuffleandlearncompose.data.retrofit
+
+import com.smtersoyoglu.shuffleandlearncompose.data.model.Word
+import retrofit2.http.GET
+
+interface WordService {
+    @GET("words.json")
+    suspend fun getWords() : List<Word>
+}
