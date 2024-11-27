94993198aca5d8573f49bffebdeddb0dffc1368e
smter <smtersoyoglu231@gmail.com>
2024-11-27 18:25:44 +0300

    Integrate BottomNavBar into WordNavGraph with navigation items

--- app/src/main/java/com/smtersoyoglu/shuffleandlearncompose/navigation/Screen.kt
+++ app/src/main/java/com/smtersoyoglu/shuffleandlearncompose/navigation/Screen.kt
@@ -1,8 +1,10 @@
 package com.smtersoyoglu.shuffleandlearncompose.navigation
 
 sealed class Screen (val route: String) {
-    data object WordMainScreen : Screen("word_main_screen")
-    data object WordDetailScreen : Screen("word_detail_screen/{wordId}") {
+    data object WordMainScreen : Screen(route = "word_main_screen")
+    data object LearnedWordsScreen : Screen(route = "learnedWordsScreen")
+    data object WordGameScreen : Screen(route = "wordGameScreen")
+    data object WordDetailScreen : Screen(route = "word_detail_screen/{wordId}") {
         fun createRoute(wordId: Int) = "word_detail_screen/$wordId"
     }
 }
--- app/src/main/java/com/smtersoyoglu/shuffleandlearncompose/navigation/WordNavGraph.kt
+++ app/src/main/java/com/smtersoyoglu/shuffleandlearncompose/navigation/WordNavGraph.kt
@@ -4,6 +4,12 @@ import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -13,42 +19,88 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.smtersoyoglu.shuffleandlearncompose.navigation.bottomnav.BottomNavBar
+import com.smtersoyoglu.shuffleandlearncompose.navigation.bottomnav.BottomNavItem
+import com.smtersoyoglu.shuffleandlearncompose.screens.learned_words.LearnedWordsScreen
 import com.smtersoyoglu.shuffleandlearncompose.screens.word_detail.WordDetailScreen
+import com.smtersoyoglu.shuffleandlearncompose.screens.word_game.WordGameScreen
 import com.smtersoyoglu.shuffleandlearncompose.screens.word_main.WordMainScreen
 
 @Composable
-fun WordNavGraph(modifier: Modifier) {
+fun WordNavGraph(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
-    NavHost(
-        navController = navController,
-        startDestination = Screen.WordMainScreen.route
-    ) {
-        composable(
+
+    val bottomNavItems = listOf(
+        BottomNavItem(
             route = Screen.WordMainScreen.route,
-            enterTransition = ::slideInToRight,
-            exitTransition = ::slideOutToLeft
-        ) {
-            WordMainScreen(navController = navController)
+            title = "Home",
+            icon = Icons.Default.Home
+        ),
+        BottomNavItem(
+            route = Screen.LearnedWordsScreen.route,
+            title = "Learned",
+            icon = Icons.Default.Star
+        ),
+        BottomNavItem(
+            route = Screen.WordGameScreen.route,
+            title = "Game", // Sekme başlığı
+            icon = Icons.Default.PlayArrow
+        )
+    )
+
+    Scaffold(
+        bottomBar = {
+            BottomNavBar(navController = navController, items = bottomNavItems)
         }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.WordMainScreen.route,
+            modifier = modifier.padding(innerPadding)
+        ) {
+            composable(
+                route = Screen.WordMainScreen.route,
+                enterTransition = ::slideInToRight,
+                exitTransition = ::slideOutToLeft
+            ) {
+                WordMainScreen(navController = navController)
+            }
 
-        composable(
-            route = Screen.WordDetailScreen.route,
-            arguments = listOf(navArgument("wordId") { type = NavType.IntType }),
-            enterTransition = ::slideInToLeft,
-            exitTransition = ::slideOutToLeft,
-            popEnterTransition = ::slideInToRight,
-            popExitTransition = ::slideOutToRight
-        ) { backStackEntry ->
-            val wordId = backStackEntry.arguments?.getInt("wordId") ?: 0
-            WordDetailScreen(
-                navController = navController,
-                wordId = wordId,
-                viewModel = hiltViewModel()
-            )
+            composable(
+                route = Screen.LearnedWordsScreen.route,
+                enterTransition = ::slideInToRight,
+                exitTransition = ::slideOutToLeft
+            ) {
+                LearnedWordsScreen(navController = navController)
+            }
 
+            composable(
+                route = Screen.WordGameScreen.route,
+                enterTransition = ::slideInToRight,
+                exitTransition = ::slideOutToLeft
+            ) {
+                WordGameScreen(navController = navController)
+            }
+
+            composable(
+                route = Screen.WordDetailScreen.route,
+                arguments = listOf(navArgument("wordId") { type = NavType.IntType }),
+                enterTransition = ::slideInToLeft,
+                exitTransition = ::slideOutToLeft,
+                popEnterTransition = ::slideInToRight,
+                popExitTransition = ::slideOutToRight
+            ) { backStackEntry ->
+                val wordId = backStackEntry.arguments?.getInt("wordId") ?: 0
+                WordDetailScreen(
+                    navController = navController,
+                    wordId = wordId,
+                    viewModel = hiltViewModel()
+                )
+            }
         }
     }
 }
+
 fun slideInToLeft(scope: AnimatedContentTransitionScope<NavBackStackEntry>): EnterTransition {
     return scope.slideIntoContainer(
         AnimatedContentTransitionScope.SlideDirection.Left,
