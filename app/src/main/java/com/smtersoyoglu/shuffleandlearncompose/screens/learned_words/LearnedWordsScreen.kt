7519e70454c4c0f3b25d6675e45f28ebbb1a935e
smter <smtersoyoglu231@gmail.com>
2024-12-13 20:53:55 +0300

    Create LearnedWordsScreen to display learned words and manage UI states

--- app/src/main/java/com/smtersoyoglu/shuffleandlearncompose/screens/learned_words/LearnedWordsScreen.kt
+++ app/src/main/java/com/smtersoyoglu/shuffleandlearncompose/screens/learned_words/LearnedWordsScreen.kt
@@ -1,23 +1,60 @@
 package com.smtersoyoglu.shuffleandlearncompose.screens.learned_words
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.smtersoyoglu.shuffleandlearncompose.navigation.Screen
+import com.smtersoyoglu.shuffleandlearncompose.screens.word_main.components.WordCard
 
 @Composable
-fun LearnedWordsScreen(navController: NavController) {
-    // Öğrenilen kelimeler listesini kodları olucak
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Learned Words", style = MaterialTheme.typography.headlineLarge)
+fun LearnedWordsScreen(
+    navController: NavController,
+    learnedWordsViewModel: LearnedWordsViewModel = hiltViewModel()
+) {
+    val learnedWordsState by learnedWordsViewModel.uiState.collectAsState()
+
+    when {
+        learnedWordsState.isLoading -> {
+            CircularProgressIndicator(modifier = Modifier.fillMaxSize())
+        }
+
+        learnedWordsState.error != null -> {
+            Text(
+                text = learnedWordsState.error ?: "Unknown error",
+                color = Color.Red,
+                modifier = Modifier.fillMaxSize(),
+                textAlign = TextAlign.Center
+            )
+        }
+
+        learnedWordsState.learnedWords.isNotEmpty() -> {
+            LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+                items(learnedWordsState.learnedWords) { word ->
+                    WordCard(wordItem = word) {
+                        navController.navigate(Screen.WordDetailScreen.createRoute(word.id, word.isLearned))
+                    }
+                }
+            }
+        }
+        else -> {
+            Text(
+                text = "No learned words yet!",
+                color = Color.Gray,
+                modifier = Modifier.fillMaxSize(),
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
