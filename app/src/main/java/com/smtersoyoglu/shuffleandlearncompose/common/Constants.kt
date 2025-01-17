dd30354ec5baec29e3b65be278a310cbffe5d67b
smter <smtersoyoglu231@gmail.com>
2025-01-17 13:29:02 +0300

    Refactor: Remove Loading state from Resource class and update BASE_URL
    Removed the Loading state from the Resource sealed class, simplifying the state management. Updated the BASE_URL in Constants to use the correct raw Github URL path for fetching the words JSON file.

--- app/src/main/java/com/smtersoyoglu/shuffleandlearncompose/common/Constants.kt
+++ app/src/main/java/com/smtersoyoglu/shuffleandlearncompose/common/Constants.kt
@@ -2,7 +2,7 @@ package com.smtersoyoglu.shuffleandlearncompose.common
 
 object Constants {
 
-    const val BASE_URL = "https://raw.githubusercontent.com/smtersoyoglu/WordsJson/main/"
+    const val BASE_URL = "https://raw.githubusercontent.com/smtersoyoglu/WordsJson/refs/heads/main/"
 
     const val DATABASE_NAME = "word_database"
 
